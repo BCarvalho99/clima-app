0cb24ca7b24d65c800366361b8462346eb9412a5
Bruno Carvalho <bcarvalho5699@gmail.com>
2023-01-26 15:47:29 -0300

    first model

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div className="w-full h-screen bg-gradient-to-br from-[#477397] via-[#1d384dfd] to-[#092031] flex flex-col items-center justify-center px-4 lg:px0">
+      {/* search */}
+      <form>search space</form>
+      <div className="shadow-xl shadow-black/80 w-full bg-white/10 max-w-[450px] min-h-[584px] text-white backdrop-blur-[32px] rounded-2xl py-12 px-6">
+        {/* top info */}
+        <div>
+          <div>info top</div>
+        </div>
+        {/* body info */}
+        <div>info body</div>
+        {/* bottom info */}
+        <div>info bottom</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
